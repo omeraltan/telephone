fbeb0c7f760250ee1636a82af4db81f51ac3bb1e
Ömer ALTAN <omeraltan66@gmail.com>
2022-06-26 20:35:08 +0300

    Initial version of telephone generated by generator-jhipster@7.8.1

---
+++ src/main/java/com/telephone/app/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.telephone.app.repository;
+
+import com.telephone.app.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ src/main/java/com/telephone/app/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.telephone.app.web.rest.vm;
